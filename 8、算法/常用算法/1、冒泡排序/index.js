4ca9a7b64a59577315cc0a2bff92b7868d4b1bcf
JimmieZhou <jimmiezhou@126.com>
2019-12-10 11:05:48 +0800

    算法-排序-冒泡

--- 8、算法/常用算法/1、冒泡排序/index.js
+++ 8、算法/常用算法/1、冒泡排序/index.js
@@ -4,18 +4,18 @@
  * @Author: jimmiezhou
  * @Date: 2019-12-10 10:56:56
  * @LastEditors: jimmiezhou
- * @LastEditTime: 2019-12-10 10:59:16
+ * @LastEditTime: 2019-12-10 11:05:32
  */
 function bubbleSort(arr) {
-    var len = arr.length;
-    for (var i = 0; i < len; i++) {
-        for (var j = 0; j < len - 1 - i; j++) {
-            if (arr[j] > arr[j + 1]) {        //相邻元素两两对比
-                var temp = arr[j + 1];        //元素交换
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
+    var len = arr.length
+    for(var i=0;i<len;i++){
+        for(var j=0;j<len -1 -i;j++){
+            if(arr[j]>arr[j+1]){
+                var temp = arr[j+1]
+                arr[j+1] = arr[j]
+                arr[j] = temp
             }
         }
     }
-    return arr;
+    return arr
 }
